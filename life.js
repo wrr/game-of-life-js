26c94d0b0de90a37ec14ad0ddcaf250cab72aa91
Jan Wrobel <wrobel@blues.ath.cx>
2011-09-19 21:08:53 +0200

    Some lint errors fixed.

--- life.js
+++ life.js
@@ -1,40 +1,64 @@
 // Copyright (C) 2011 Jan Wrobel <wrobel@blues.ath.cx>
 
-CELL_SIZE_PX=15;
+"use strict";
+
+var CELL_SIZE_PX = 15;
 
 function createTable(height, width) {
-  var space = document.getElementById("space");
-  for (var i = 0; i < height; ++i) {
-    var row = space.insertRow(-1);
-    for (var j = 0; j < width; ++j) {
-      var cell = row.insertCell(j);
+  var space, row, cell, i, j;
+  space = document.getElementById("space");
+  for (i = 0; i < height; i += 1) {
+    row = space.insertRow(-1);
+    for (j = 0; j < width; j += 1) {
+      cell = row.insertCell(j);
       cell.style.width = (CELL_SIZE_PX - 1) + "px";
       cell.style.height = (CELL_SIZE_PX - 1) + "px";
     }
   }
 }
 
-function space(height, width) {
-  this.height = height;
-  this.width = width;
-  this.space = create2dArray(height, width);
-  randomizeSpace(this.space, height, width);
-  this.newSpace = create2dArray(height, width);
+function create2dArray(height) {
+  var result, i;
+  result = [];
+  for (i = 0; i < height; i += 1) {
+    result[i] = [];
+  }
+  return result;
+}
 
-  this.canChangeAdded = create2dArray(height, width);
-  this.canChange = new Array();
-  for (var i = 1; i < height - 1; ++i) {
-    for (var j = 1; j < width - 1; ++j) {
+function randomizeSpace(space, height, width) {
+  var i, j;
+  for (i = 1; i < height - 1; i += 1) {
+    for (j = 1; j < width - 1; j += 1) {
+      if (Math.floor(Math.random() * 11) % 10 === 0) {
+        space[i][j] = 1;
+      } else {
+        space[i][j] = 0;
+      }
+    }
+  }
+}
+
+function space(height, width) {
+  var i, j;
+  this.space = create2dArray(height);
+  randomizeSpace(this.space, height, width);
+  this.newSpace = create2dArray(height);
+
+  this.canChangeAdded = create2dArray(height);
+  this.canChange = [];
+  for (i = 1; i < height - 1; i += 1) {
+    for (j = 1; j < width - 1; j += 1) {
       this.canChange.push([i,j]);
     }
   }
-  this.newCanChange = new Array();
+  this.newCanChange = [];
 
   this.addNeighborhoodToNewCanChange = function(row, col) {
-    for (var i = -1; i <= 1; ++i) {
-      for (var j = -1; j <= 1; ++j) {
-        if (row + i > 0 && row + i < this.height - 1 &&
-            col + j > 0 && col + j < this.width - 1 &&
+    for (i = -1; i <= 1; i += 1) {
+      for (j = -1; j <= 1; j += 1) {
+        if (row + i > 0 && row + i < height - 1 &&
+            col + j > 0 && col + j < width - 1 &&
             !this.canChangeAdded[row + i][col + j]) {
           this.canChangeAdded[row + i][col + j] = true;
           this.newCanChange.push([row + i, col + j]);
@@ -48,17 +72,19 @@ function space(height, width) {
   }
 
   this.evolveSpaceImpl = function(space, newSpace) {
-    for (var k = 0; k < this.canChange.length; ++k) {
-      var i = this.canChange[k][0];
-      var j = this.canChange[k][1];
-      var neighboursCount = space[i - 1][j - 1]
-                          + space[i - 1][j]
-                          + space[i - 1][j + 1]
-                          + space[i][j - 1]
-                          + space[i][j + 1]
-                          + space[i + 1][j - 1]
-                          + space[i + 1][j]
-                          + space[i + 1][j + 1];
+    var k, i, j, neighboursCount, tmpCanChange, obj, callback;
+
+    for (k = 0; k < this.canChange.length; k += 1) {
+      i = this.canChange[k][0];
+      j = this.canChange[k][1];
+      neighboursCount = space[i - 1][j - 1]
+                      + space[i - 1][j]
+                      + space[i - 1][j + 1]
+                      + space[i][j - 1]
+                      + space[i][j + 1]
+                      + space[i + 1][j - 1]
+                      + space[i + 1][j]
+                      + space[i + 1][j + 1];
       if (neighboursCount == 2 && space[i][j] == 1) {
         newSpace[i][j] = 1;
       } else if (neighboursCount == 3) {
@@ -70,19 +96,19 @@ function space(height, width) {
         this.addNeighborhoodToNewCanChange(i, j);
       }
     }
-    for (var k = 0; k < this.newCanChange.length; ++k) {
-      var i = this.newCanChange[k][0];
-      var j = this.newCanChange[k][1];
+    for (k = 0; k < this.newCanChange.length; k += 1) {
+      i = this.newCanChange[k][0];
+      j = this.newCanChange[k][1];
       this.canChangeAdded[i][j] = false;
     }
-    displaySpace(this.newCanChange, space, newSpace, this.height, this.width);
-    var tmpCanChange = this.canChange;
+    displaySpace(this.newCanChange, space, newSpace, height, width);
+    tmpCanChange = this.canChange;
     this.canChange = this.newCanChange;
     this.newCanChange = tmpCanChange;
     this.newCanChange.length = 0;
 
-    var obj = this;
-    var callback = function() {
+    obj = this;
+    callback = function() {
       obj.evolveSpaceImpl(newSpace, space);
     }
     setTimeout(callback, 100);
@@ -90,13 +116,14 @@ function space(height, width) {
 }
 
 function displaySpace(changed, oldSpace, newSpace, height, width) {
-  var table = document.getElementById("space");
-  for (var k = 0; k < changed.length; ++k) {
-    var i = changed[k][0];
-    var j = changed[k][1];
+  var table, row, cell, k, i, j;
+  table = document.getElementById("space");
+  for (k = 0; k < changed.length; k += 1) {
+    i = changed[k][0];
+    j = changed[k][1];
     if (oldSpace[i][j] != newSpace[i][j]) {
-      var row = table.rows[i];
-      var cell = row.cells[j];
+      row = table.rows[i];
+      cell = row.cells[j];
       if (newSpace[i][j] == 1) {
         cell.style.background="gray";
       } else {
@@ -106,54 +133,6 @@ function displaySpace(changed, oldSpace, newSpace, height, width) {
   }
 }
 
-function create2dArray(height, width) {
-  var result = new Array(height);
-  for (var i = 0; i < height; ++i) {
-    result[i] = new Array(width);
-  }
-  return result;
-}
-
-function randomizeSpace(space, height, width) {
-  for (var i = 1; i < height - 1; ++i) {
-    for (var j = 1; j < width - 1; ++j) {
-      if (Math.floor(Math.random()*11) % 10 == 0) {
-        space[i][j] = 1;
-      } else {
-        space[i][j] = 0;
-      }
-    }
-  }
-}
-
-function evolveSpace(space, newSpace, height, width) {
-  for (var i = 1; i < height - 1; ++i) {
-    for (var j = 1; j < width - 1; ++j) {
-      var neighboursCount = space[i - 1][j - 1]
-                          + space[i - 1][j]
-                          + space[i - 1][j + 1]
-                          + space[i][j - 1]
-                          + space[i][j + 1]
-                          + space[i + 1][j - 1]
-                          + space[i + 1][j]
-                          + space[i + 1][j + 1];
-      if (neighboursCount == 2 && space[i][j] == 1) {
-        newSpace[i][j] = 1;
-      } else if (neighboursCount == 3) {
-        newSpace[i][j] = 1;
-      } else {
-        newSpace[i][j] = 0;
-      }
-    }
-  }
-  displaySpace(space, newSpace, height, width);
-  clearSpace(space);
-  var callback = function() {
-    evolveSpace(newSpace, space, height, width);
-  }
-  setTimeout(callback, 100);
-}
-
 function getWindowWidthAndHeight() {
   // the more standards compliant browsers
   // (mozilla/netscape/opera/IE7) use window.innerWidth and
@@ -161,7 +140,7 @@ function getWindowWidthAndHeight() {
   if (typeof window.innerWidth != 'undefined') {
     return [window.innerWidth, window.innerHeight];
   }
- 
+
   // IE6 in standards compliant mode (i.e. with a valid doctype as the
   // first line in the document)
   if (typeof document.documentElement != 'undefined'
@@ -190,4 +169,3 @@ var height = Math.floor(getWindowHeight() / CELL_SIZE_PX) - 1;
 createTable(height, width);
 var space = new space(height, width);
 space.evolveSpace();
-
